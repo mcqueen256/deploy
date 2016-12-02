c5f2b4a60f6e691c94274491498fadf56ffbde97
mcqueen256 <bucknich@gmail.com>
2016-12-02 18:22:06 +1000

    can deploy and delete server

---
+++ resources/app.wsgi
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+activate_file = '/var/www/{app_name}/{app_name}/venv/bin/activate_this.py'
+with open(activate_file) as file_:
+    exec(file_.read(), dict(__file__=activate_file))
+
+import sys
+import logging
+logging.basicConfig(stream=sys.stderr)
+sys.path.insert(0,"/var/www/{app_name}/")
+
+sys.stderr.write(sys.version)
+
+from FlaskApp import app as application
+application.secret_key = '{secret_key}'
+
--- webserver.py
+++ webserver.py
@@ -2,6 +2,10 @@
 
 import os
 import sys
+import shutil
+import random
+import string
+import subprocess
 
 def error(message):
     print(message)
@@ -10,13 +14,13 @@ def error(message):
 def deploy(args):
     '''
     Create a new web server with given name.
-    Usage: webserver deploy APPLICATION_NAME
+    Usage: webserver deploy APPLICATION_NAME APPLICATION_DOMAIN
     '''
-    print("args", args)
-    if len(args) != 1:
-        error('invalid arguments.\nusage: ' + sys.argv[0] + ' deploy APPLICATION_NAME')
+    if len(args) < 2:
+        error('invalid arguments.\nusage: ' + sys.argv[0] + ' deploy APPLICATION_NAME APPLICATION_DOMAIN [options ...]')
     # get the application name
     app_name = args[0]
+    server_name = args[1]
     # check that there are only numbers, letters and underscores
     def check_valid_char(char):
         result = False
@@ -31,7 +35,67 @@ def deploy(args):
         if not check_valid_char(character):
             error('invalid name ' + app_name + ', must only contain numbers, letters, understors, minus signs or periods.')
     # check the application does not already exist
+    if app_name in os.listdir('/var/www'):
+        error('application {} already exists in /var/www/'.format(app_name))
+    # settings for the server
+    settings = {
+        'server_name':server_name,
+        'admin_email':'admin@nicbuckeridge.com',
+        'app_name': app_name,
+        'user': 'nicholas',
+        'group': 'nicholas',
+        'threads': '5'
+    }
+    for arg in args[2:]:
+        if len(arg.split('=')) > 2:
+            error('option \'{}\' is not valid'.format())
+        key, value = tuple(arg.split("="))
+        if key not in settings.keys():
+            error('invalid option \'{}\''.format(key))
+    # create application
+    script_path = os.path.dirname(os.path.realpath(__file__))
+    resource_path = script_path + '/resources/'
+    print('resource path', resource_path)
+    # create parent directory
+    os.mkdir('/var/www/{}'.format(app_name))
+    # create wsgi app
+    with open(resource_path + 'app.wsgi', 'r') as fin:
+        with open('/var/www/{app_name}/{app_name}.wsgi'.format(app_name=app_name), 'w') as fout:
+            fout.write(fin.read().format(app_name=app_name, secret_key="".join([random.choice(string.ascii_letters+string.digits) for x in range(64)])))
+    # create application directory
+    os.mkdir('/var/www/{app_name}/{app_name}'.format(app_name=app_name))
+    # create default directories
+    os.mkdir('/var/www/{app_name}/{app_name}/static'.format(app_name=app_name))
+    os.mkdir('/var/www/{app_name}/{app_name}/templates'.format(app_name=app_name))
+    # Create the init file
+    shutil.copy(resource_path + '__init__.py', '/var/www/{app_name}/{app_name}/__init__.py'.format(app_name=app_name))
+    # create virtula evironment
+    subprocess.run(['pyvenv', '/var/www/{app_name}/{app_name}/venv'.format(app_name=app_name)])
+    # create wsgi mapping
+    with open(resource_path + 'app.conf', 'r') as fin:
+        with open('/etc/apache2/sites-available/{app_name}.conf'.format(app_name=app_name), 'w') as fout:
+            fout.write(fin.read().format(**settings))
+    # inform the user of the possibility of a server restart
 
+
+def delete(args):
+    '''
+    Deletes a flask application form the disk.
+    Usage: webserver delete APPLICTION_NAME
+    '''
+    if len(args) != 1:
+        error('invalid arguments.\nusage: ' + sys.argv[0] + ' delete APPLICATION_NAME')
+    # get the application name
+    app_name = args[0]
+    if app_name not in os.listdir('/var/www'):
+        error('application {} does not exist'.format(app_name))
+    print('warning: this will remove the application perminately from the disk.')
+    i = input('are you sure? (y/n)')
+    if i == 'y':
+        print('deleting {}'.format(app_name))
+        os.remove('/etc/apache2/sites-available/{app_name}.conf'.format(app_name=app_name))
+        shutil.rmtree('/var/www/{}'.format(app_name))
+    exit(0)
 
 
 
@@ -39,6 +103,7 @@ def main():
     # List of functions
     operations = {
         'deploy': deploy,
+        'delete': delete,
     }
 
     # run command
